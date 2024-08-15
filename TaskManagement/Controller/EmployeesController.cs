e7cba19a4af5e11d23d2e202d79c54e5491c6554
dhananjayab198 <dhananjayab198@gmail.com>
2024-08-15 18:50:33 +0530

    Initial commit check

---
+++ TaskManagement/Controller/EmployeesController.cs
@@ -0,0 +1,96 @@
+﻿// Controllers/EmployeesController.cs
+using Microsoft.AspNetCore.Mvc; 
+using TaskManagement.Interfaces;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeesController(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Employee>>> GetEmployees()
+    {
+        List<Employee> employees = await _employeeRepository.GetAllEmployeesAsync();
+        if (employees == null)
+        {
+            return NotFound();
+        }
+        return Ok(employees);
+    }
+
+    [HttpGet("{name}")]
+    public async Task<ActionResult<Employee>> GetEmployee([FromRoute]string name)
+    {
+        Employee employee = await _employeeRepository.GetEmployeeAsync(name);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        return employee;
+    }
+
+    [HttpGet("manager/{managerName}")]
+    public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesByManagerName([FromRoute] string managerName)
+    {
+        List<Employee> employees = await _employeeRepository.GetEmployeesByManagerNameAsync(managerName);
+
+        if (employees == null)
+        {
+            return NotFound();
+        }
+
+        return employees;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddEmployee([FromBody]Employee employee)
+    {
+        if(employee == null)
+        {
+            return BadRequest("Need minimum data");
+        }
+        var isEmployeeAdded = await _employeeRepository.AddEmployeeAsync(employee);
+        if (!isEmployeeAdded)
+        {
+            return BadRequest("Failed to create employee");
+        }
+        return Ok("Employee Added Successfully");
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateEmployee([FromRoute]Guid id,[FromBody] Employee employee)
+    {
+        if (id != employee.Id)
+        {
+            return BadRequest();
+        }
+
+        var isUpdatedEmployee = await _employeeRepository.UpdateEmployeeAsync(employee);
+
+        if (!isUpdatedEmployee)
+        {
+            return BadRequest("Update Failure");
+        }
+         
+        return Ok("Updated successfully");
+    }
+
+    [HttpDelete("{employeeName}")]
+    public async Task<IActionResult> DeleteEmployee([FromRoute] string employeeName)
+    {
+        var employee = await _employeeRepository.DeleteEmployeeAsync(employeeName);
+        if (!employee)
+        {
+            return NotFound("Employee not found");
+        }
+        return Ok("Deleted successfully");        
+    }
+}
---
+++ TaskManagement/Controller/TasksController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Interfaces;
+
+namespace TaskManagement.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ITaskRepository _taskRepository;
+        public TasksController(IEmployeeRepository employeeRepository,ITaskRepository taskRepository)
+        {
+            this._employeeRepository = employeeRepository;
+            this._taskRepository = taskRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAllTasks()
+        {
+            List<Task> tasks = await _taskRepository.GetAllTasksAsync();
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+            return Ok(tasks);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Task>> GetTask([FromRoute] Guid Id)
+        {
+            Task task = await _taskRepository.GetTaskIdAsync(Id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+         
+        [HttpPost]
+        public async Task<ActionResult> AddTask([FromBody] Task task)
+        {
+            if (task == null)
+            {
+                return BadRequest("Bad data of task");
+            }
+            var istaskAdded = await _taskRepository.AddTaskAsync(task);
+            if (!istaskAdded)
+            {
+                return BadRequest("Failed to create task");
+            }
+            return Ok("Task Added Successfully");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask([FromRoute] Guid id, [FromBody] Task task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            var isUpdatedEmployee = await _taskRepository.UpdateTaskAsync(task);
+
+            if (!isUpdatedEmployee)
+            {
+                return BadRequest("task Update Failure");
+            }
+
+            return Ok("task Updated successfully");
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid Id)
+        {
+            var task = await _taskRepository.DeleteTaskAsync(Id);
+            if (!task)
+            {
+                return NotFound("task not found");
+            }
+            return Ok("task successfully");
+        }
+    }
+}
